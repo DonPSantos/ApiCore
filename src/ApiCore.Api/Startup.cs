d2444782045117a02aa2405a962780ede87101ac
Don Santos <donbrendon@hotmail.com>
2021-03-07 22:26:22 -0300

    Configuração de ambinetes e secrets

--- src/ApiCore.Api/Startup.cs
+++ src/ApiCore.Api/Startup.cs
@@ -15,12 +15,23 @@ namespace ApiCore.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
+
+        public Startup(IHostEnvironment hostEnvironment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            if (hostEnvironment.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
